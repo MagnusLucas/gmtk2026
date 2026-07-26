057b76bee32e0d16ad413706f3d8a6c5a292d34e
Zapzap <zuzannaa.pietrzak@gmail.com>
2026-07-26 17:13:16 +0200

    yea I think cmobo works
    I think

--- data/unit_resource.gd
+++ data/unit_resource.gd
@@ -4,3 +4,7 @@ class_name UnitAttributes
 @export var name : String
 @export var attacks : Array[AttackResource]
 @export var animated_texture_frames : SpriteFrames
+@export_category('combo')
+@export var combo_cooldown_time := 1 
+@export var combo_index := 1
+@export var combo_multiplier := 2
--- scenes/player_unit/player_unit.gd
+++ scenes/player_unit/player_unit.gd
@@ -9,17 +9,20 @@ var target: Node2D
 var bullets_to_spawn := 0
 var strength_modifier := 0.0
 var active_attack: AttackResource
+var active_combo_modifier:= 1
 
 @onready var bullet_spawn_interval_timer: Timer = $BulletSpawnIntervalTimer
 
 
+
 func _ready() -> void:
+	$ComboTimer.wait_time = stats.combo_cooldown_time
 	$AnimatedSprite2D.sprite_frames = stats.animated_texture_frames
 	bullet_spawn_interval_timer.timeout.connect(_on_timer_timeout)
 
-
 # This setup will spawn bullets from new active attack if any are remaining,
 # but I don't think it'll be a problem in this project
+
 func attack(attack_strength_modifier: float = 1):
 	strength_modifier = attack_strength_modifier
 	
@@ -28,12 +31,28 @@ func attack(attack_strength_modifier: float = 1):
 	target = get_tree().get_nodes_in_group("enemy")[0]
 	
 	bullets_to_spawn += active_attack.bullet_amount
+	if !$ComboTimer.is_stopped():
+		active_combo_modifier = stats.combo_multiplier
+	else:
+		active_combo_modifier = 1 
+	
 	bullet_spawn_interval_timer.start(active_attack.bullet_interval)
-
+	for player_unit in get_tree().get_nodes_in_group('player_units'):
+		player_unit.check_combo(stats.combo_index)
 
 func _on_timer_timeout() -> void:
 	BulletManager.Instance.spawn_bullet(active_attack.bullet, global_position,
-			target, strength_modifier)
+		target, strength_modifier*active_combo_modifier)
 	bullets_to_spawn -= 1
 	if bullets_to_spawn == 0:
 		bullet_spawn_interval_timer.stop()
+
+func check_combo(combo_index_of_unit):
+	#print(combo_index_of_unit,' szescsiedemmmm ', stats.combo_index)
+	##w rosnącej kolejności powinno być combo
+	# please, refrain from questions about -1, its an artefact that has become a friend.
+	# accept -1 as a starting point in your future
+	if combo_index_of_unit == stats.combo_index-1 || (combo_index_of_unit == -1 && stats.combo_index == 1 ):
+		$ComboTimer.start()
+	else:
+		$ComboTimer.stop()
