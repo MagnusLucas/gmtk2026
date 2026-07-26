775ff38763c4edb42aa6009ffa4592b9f950ecf1
MagnusLucas <lukasz.kisielewski@student.uj.edu.pl>
2026-07-26 17:47:55 +0200

    Playing animation on icon when shooting

--- scenes/player_unit/player_unit.gd
+++ scenes/player_unit/player_unit.gd
@@ -12,11 +12,12 @@ var active_attack: AttackResource
 var active_combo_modifier:= 1
 
 @onready var bullet_spawn_interval_timer: Timer = $BulletSpawnIntervalTimer
+@onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
 
 
 func _ready() -> void:
 	$ComboTimer.wait_time = stats.combo_cooldown_time
-	$AnimatedSprite2D.sprite_frames = stats.animated_texture_frames
+	animated_sprite_2d.sprite_frames = stats.animated_texture_frames
 	bullet_spawn_interval_timer.timeout.connect(_on_timer_timeout)
 
 # This setup will spawn bullets from new active attack if any are remaining,
@@ -30,6 +31,7 @@ func attack(attack_strength_modifier: float = 1):
 	target = get_tree().get_nodes_in_group("enemy")[0]
 	
 	bullets_to_spawn += active_attack.bullet_amount
+	animated_sprite_2d.play()
 	if !$ComboTimer.is_stopped():
 		active_combo_modifier = stats.combo_multiplier
 	else:
@@ -39,12 +41,16 @@ func attack(attack_strength_modifier: float = 1):
 	for player_unit in get_tree().get_nodes_in_group('player_units'):
 		player_unit.check_combo(stats.combo_index)
 
+
 func _on_timer_timeout() -> void:
 	BulletManager.Instance.spawn_bullet(active_attack.bullet, global_position,
 		target, strength_modifier*active_combo_modifier)
 	bullets_to_spawn -= 1
 	if bullets_to_spawn == 0:
 		bullet_spawn_interval_timer.stop()
+		animated_sprite_2d.stop()
+		
+
 
 func check_combo(combo_index_of_unit):
 	#print(combo_index_of_unit,' szescsiedemmmm ', stats.combo_index)
