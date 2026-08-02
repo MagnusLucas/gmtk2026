cc21a48a3e6a525cf39b7db68b142c13f2276a53
MagnusLucas <lukasz.kisielewski@student.uj.edu.pl>
2026-08-02 19:56:01 +0200

    Sound plays also  when beat starts

--- scenes/beat_player.gd
+++ scenes/beat_player.gd
@@ -2,6 +2,7 @@ class_name BeatPlayer
 extends AudioStreamPlayer
 
 const BEAT_PLAYER = preload("res://scenes/beat_player.tscn")
+const MAX_POLYPHONY := 3
 
 var beat: Beat
 
@@ -10,16 +11,23 @@ var beat: Beat
 static func custom_new(a_beat: Beat) -> BeatPlayer:
 	var instance: BeatPlayer = BEAT_PLAYER.instantiate()
 	instance.stream = a_beat.get_audio()
+	instance.max_polyphony = MAX_POLYPHONY
 	instance.beat = a_beat
 	instance.ready.connect(instance.set_interval.bind(a_beat.interval), CONNECT_ONE_SHOT)
-	instance.beat.died.connect(instance.queue_free, CONNECT_ONE_SHOT)
+	instance.beat.died.connect(instance._die_when_sound_finishes_playing, CONNECT_ONE_SHOT)
 	return instance
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	play()
 	timer.timeout.connect(play)
 
 
 func set_interval(time: float) -> void:
 	timer.start(time)
+
+
+func _die_when_sound_finishes_playing() -> void:
+	timer.stop()
+	finished.connect(queue_free, CONNECT_ONE_SHOT)
