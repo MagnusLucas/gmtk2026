cd146874bb9ac6946e8933946a6662f88341410c
MagnusLucas <lukasz.kisielewski@student.uj.edu.pl>
2026-08-06 12:19:01 +0200

    Fixed win text not animating

--- scenes/player_unit.gd
+++ scenes/player_unit.gd
@@ -7,12 +7,18 @@ extends Sprite2D
 
 
 func _ready() -> void:
-	if bullet_spawn_point:
+	if bullet_spawn_point and not Engine.is_editor_hint():
 		stats.bullet_spawn_point = bullet_spawn_point.global_position
+	_update_visuals()
 
 
 func _set_stats(value: UnitAttributes) -> void:
 	stats = value
-	texture = stats.texture
 	if bullet_spawn_point:
 		stats.bullet_spawn_point = bullet_spawn_point.global_position
+	_update_visuals()
+
+
+func _update_visuals() -> void:
+	if stats:
+		texture = stats.texture
