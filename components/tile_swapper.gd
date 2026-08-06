a567c6ee22d46a96a8d35b3dfcefe6cd3b79cfdc
MagnusLucas <lukasz.kisielewski@student.uj.edu.pl>
2026-08-06 15:49:23 +0200

    Added swaping tiles sound effect and a bit more text tutorial

--- components/tile_swapper.gd
+++ components/tile_swapper.gd
@@ -2,6 +2,7 @@ class_name TileSwapper
 extends Node
 
 @export var board: Board
+@export var audio_stream_player: AudioStreamPlayer
 
 var marked_tile: Tile
 var marked_tile_position: Vector2i
@@ -9,6 +10,10 @@ var marked_tile_position: Vector2i
 
 func mark_tile(tile: Tile, tile_position: Vector2i) -> void:
 	if marked_tile:
+		if marked_tile == tile:
+			marked_tile.set_marked(false)
+			return
+		
 		marked_tile.set_marked(false)
 		var marked_tile_data := marked_tile.get_tile_data()
 		board.set_tile(tile.get_tile_data(), marked_tile_position)
@@ -16,6 +21,8 @@ func mark_tile(tile: Tile, tile_position: Vector2i) -> void:
 		marked_tile.set_tile_data(tile.get_tile_data())
 		tile.set_tile_data(marked_tile_data)
 		marked_tile = null
+		if audio_stream_player:
+			audio_stream_player.play()
 	else:
 		marked_tile = tile
 		marked_tile_position = tile_position
